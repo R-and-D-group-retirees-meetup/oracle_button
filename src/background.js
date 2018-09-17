9d69621d946adeeae086cf96a672be4222042ebd
Mic-U <yuho.miki@gmail.com>
2018-09-17 14:26:57 +0900

    設定値を localStorage に保存 chrome.storage.local でも良かったが、コールバックが辛いので localStorage を選択

--- src/background.js
+++ src/background.js
@@ -1,22 +1,88 @@
 "use strict";
 
-let current = "kiritan";
-let isOracleButtonVisible = true;
+/**
+ * @typedef {Object} Config
+ * @property {string} voice
+ * @property {boolean} isOracleButtonVisible
+ */
 
+/**
+ * @type {Config}
+ */
+const DEFAULT_CONFIG = {
+	voice: "kiritan",
+	isOracleButtonVisible: true
+};
+Object.freeze(DEFAULT_CONFIG);
+
+const LOCAL_STORAGE_KEY = "oracle_button_config";
+/**
+ * 
+ * @param {Object} config 
+ * @returns {boolean}
+ */
+function isValidConfig(config) {
+	return config.hasOwnProperty("voice") && config.hasOwnProperty("isOracleButtonVisible");
+}
+
+/**
+ * @returns {Config}
+ */
+function getConfig() {
+	const config = localStorage.getItem(LOCAL_STORAGE_KEY);
+	if (!config) {
+		return DEFAULT_CONFIG;
+	}
+
+	try {
+		const parsedConfig = JSON.parse(config);
+		return isValidConfig(parsedConfig) ? parsedConfig : DEFAULT_CONFIG;
+	} catch {
+		console.warn("Unexpected Config structure");
+		return  DEFAULT_CONFIG;
+	}
+	
+}
+
+/**
+ * @param {Config} config
+ */
+function setConfig(config) {
+	localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(config));
+}
+
+/**
+ * @returns {string}
+ */
 function getCurrentVoice() {
-	return current;
+	const config = getConfig();
+	return config.voice;
 }
 
+/**
+ * @param {string} selected
+ */
 function setCurrentVoice(selected) {
-	current = selected;
+	const config = getConfig();
+	setConfig({
+		voice: selected,
+		isOracleButtonVisible: config.isOracleButtonVisible
+	});
 }
 
+/**
+ * @returns {boolean}
+ */
 function getOracleButtonVisibility() {
-	return isOracleButtonVisible;
+	return getConfig().isOracleButtonVisible;
 }
 
 function setOracleButtonVisiblity(shouldBeVisible) {
-	isOracleButtonVisible = shouldBeVisible;
+	const config = getConfig();
+	setConfig({
+		voice: config.voice,
+		isOracleButtonVisible: shouldBeVisible
+	});
 	sendToggleMessage(shouldBeVisible, () => {
 			return;
 	});
@@ -33,6 +99,7 @@ function sendToggleMessage(shouldBeVisible, callback) {
 };
 
 function say() {
+	const current = getConfig().voice;
 	const audioURL = chrome.extension.getURL(`sounds/${current}/oracle.wav`);
 	const audio = new Audio();
 	audio.src = audioURL;
@@ -51,3 +118,5 @@ chrome.runtime.onMessage.addListener(
 			});
 		}
 });
+
+
